234c0d617be25189149b175ce1169ddb0011840a
student-ailover <ayushsule1036@gmail.com>
2025-08-14 20:26:38 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#include <stdlib.h>
+
+char symbols[3][3] = {
+    {' ', ' ', ' '},
+    {' ', ' ', ' '},
+    {' ', ' ', ' '}
+};
+int symbolCount;
+char nextSymbol, winnerSymbol;
+int boxLen;
+RECT rcArea;
+
+char windowText[50];
+
+char playerName[2][20] = {"Ayush", "Amol"};
+
+BOOL fGameOver;
+
+POINT lineStart, lineEnd;
+
+void InitGame(HWND hWnd){
+
+    fGameOver = FALSE;
+    nextSymbol = rand() % 2? 'X' : 'O';
+
+    const char *firstPlayer;
+    firstPlayer = nextSymbol == 'X'? playerName[0] : playerName[1];
+    strcpy(windowText, firstPlayer);
+    strcat(windowText, " will be the first one to play");
+    SetWindowText(hWnd, windowText);
+
+    symbolCount = 0;
+    memset(&lineStart, 0, sizeof(POINT));
+    memset(&lineEnd, 0, sizeof(POINT));
+    memset(symbols, ' ', 9);
+
+}
+
+void CheckIfGameIsOver(HWND hWnd){
+
+    // rows
+    for(int row = 0; row < 3; row++){
+        if(symbols[row][0] == symbols[row][1] && symbols[row][1] == symbols[row][2]
+         && symbols[row][0] != ' '){
+            winnerSymbol = symbols[row][0];
+            lineStart.x = rcArea.left;
+            lineStart.y = rcArea.top + boxLen * row + boxLen / 2;
+            lineEnd.x = rcArea.right;
+            lineEnd.y = lineStart.y;
+            fGameOver = TRUE;
+        }
+    }
+
+    // columns
+    for(int col = 0; col < 3; col++){
+        if(symbols[0][col] == symbols[1][col] && symbols[1][col] == symbols[2][col]
+         && symbols[0][col] != ' '){
+            winnerSymbol = symbols[0][col];
+            lineStart.x = rcArea.left + boxLen * col + boxLen / 2;
+            lineStart.y = rcArea.top;
+            lineEnd.x = lineStart.x;
+            lineEnd.y = rcArea.bottom;
+            fGameOver = TRUE;
+        }
+    }
+
+    // diagonals
+    if(symbols[0][0] == symbols[1][1] && symbols[1][1] == symbols[2][2]
+        && symbols[0][0] != ' '){
+
+        winnerSymbol = symbols[0][0];
+        lineStart.x = rcArea.left;
+        lineStart.y = rcArea.top;
+        lineEnd.x = rcArea.right;
+        lineEnd.y = rcArea.bottom;
+        fGameOver = TRUE;
+        
+    }
+
+    else if(symbols[0][2] == symbols[1][1] && symbols[1][1] == symbols[2][0]
+        && symbols[0][2] != ' '){
+
+        winnerSymbol = symbols[0][2];
+        lineStart.x = rcArea.right;
+        lineStart.y = rcArea.top;
+        lineEnd.x = rcArea.left;
+        lineEnd.y = rcArea.bottom;
+        fGameOver = TRUE;
+        
+    }
+
+    else if(symbolCount == 9){
+        winnerSymbol = ' ';
+        fGameOver = TRUE;
+    }
+
+    if(!fGameOver) return;
+
+    RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_ERASE | RDW_UPDATENOW);
+
+    const char *winnerName;
+
+    switch(winnerSymbol){
+
+        case 'X': winnerName = playerName[0];
+            break;
+
+        case 'O': winnerName = playerName[1];
+            break;
+
+        case ' ':
+            SetWindowText(hWnd, "DRAW");
+            return;
+
+    }
+
+    strcpy(windowText, winnerName);
+    strcat(windowText, " is the winner");
+    SetWindowText(hWnd, windowText);
+
+    strcpy(windowText, "Congratulations, ");
+    strcat(windowText, winnerName);
+    strcat(windowText, "!");
+
+    if(!PlaySound("victory.wav", NULL, SND_FILENAME | SND_ASYNC))
+        printf("unable to play sound\n");
+    
+    MessageBox(hWnd, "You are the winner", windowText, MB_OK);
+
+}
+
+#include "window_proc.h"
+
+int APIENTRY WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR cmdLine,
+    int nCmdShow
+)
+{
+
+    WNDCLASS wc = {0};
+    wc.hbrBackground = CreateSolidBrush(RGB(255, 0, 0));
+    wc.lpszClassName = "My Window Class";
+    wc.lpfnWndProc = WindowProc;
+
+    if(!RegisterClass(&wc))
+    {
+        MessageBox(NULL, "UNABLE TO REGISTER CLASS", NULL, MB_OK);
+        return FALSE;
+    }
+
+    HWND hWnd = CreateWindow(
+        "My Window Class",
+        "Tic Tac Toe",
+        WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_BORDER | WS_CAPTION | WS_SYSMENU | WS_SIZEBOX,
+        50, 50, 500, 400,
+        NULL, NULL, NULL, NULL
+    );
+    if(!hWnd)
+    {
+        MessageBox(NULL, "UNABLE TO CREATE WINDOW", NULL, MB_OK);
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg;
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+
+}
---
+++ window_proc.h
@@ -0,0 +1,171 @@
+HPEN hPen;
+HPEN hGameOverPen;
+HFONT hFont;
+
+LRESULT CALLBACK WindowProc(
+    HWND hWnd,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+){
+
+    switch (uMsg)
+    {
+    case WM_CREATE:{
+
+        srand(time(0));
+        InitGame(hWnd);
+        hPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 0));
+        hGameOverPen = CreatePen(PS_SOLID, 15, RGB(0, 0, 0));
+
+        return 0;
+    }
+
+    case WM_SIZE:{
+
+        if(hFont) DeleteObject(hFont);
+
+        boxLen = (LOWORD(lParam) / 3 < HIWORD(lParam) / 3) ? (LOWORD(lParam) / 3) : (HIWORD(lParam) / 3);
+        rcArea.left = (LOWORD(lParam) - boxLen * 3) / 2;
+        rcArea.top = (HIWORD(lParam) - boxLen * 3) / 2;
+        rcArea.right = rcArea.left + boxLen * 3;
+        rcArea.bottom = rcArea.top + boxLen * 3;
+
+        hFont = CreateFont(boxLen - 10,0,0,0,FW_DONTCARE,FALSE,FALSE,FALSE,DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
+                CLIP_DEFAULT_PRECIS,CLEARTYPE_QUALITY, VARIABLE_PITCH,TEXT("Impact"));
+
+        SelectObject(GetDC(hWnd), hFont);
+
+        RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW | RDW_ERASE);
+
+        return 0;
+    }
+
+    case WM_LBUTTONDOWN:{
+
+        if(fGameOver){
+            InitGame(hWnd);
+            RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
+            return 0;    
+        }
+
+        POINT cursorPos;
+        cursorPos.x = LOWORD(lParam);
+        cursorPos.y = HIWORD(lParam);
+
+        // if cursor is outside the boxes
+        if(!PtInRect(&rcArea, cursorPos)) return 0;
+
+        int row = (cursorPos.y - rcArea.top) / boxLen;
+        int col = (cursorPos.x - rcArea.left) / boxLen;
+        
+        if(symbols[row][col] != ' ') return 0;
+
+        symbols[row][col] = nextSymbol;
+        symbolCount++;
+        
+        CheckIfGameIsOver(hWnd);
+        RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
+
+        if(!fGameOver){
+            nextSymbol = nextSymbol == 'X'? 'O' : 'X';
+            const char *nextPlayerName = nextSymbol == 'X' ? playerName[0] : playerName[1];
+            strcpy(windowText, "Now, it is ");
+            strcat(windowText, nextPlayerName);
+            strcat(windowText, "'s turn");
+            SetWindowText(hWnd, windowText);
+        }
+
+        return 0;
+    }
+
+    case WM_ERASEBKGND:{
+
+        HDC hDC = (HDC) wParam;
+        HBRUSH hBackgroundBrush = CreateSolidBrush(RGB(255, 0, 0));
+
+        RECT rcClient;
+        GetClientRect(hWnd, &rcClient);
+
+        RECT rcLeft;
+        rcLeft.left = rcClient.left;
+        rcLeft.top = rcClient.top;
+        rcLeft.right = rcArea.left + 1;        
+        rcLeft.bottom = rcClient.bottom;
+
+        RECT rcRight;
+        rcRight.left = rcArea.right;
+        rcRight.top = rcClient.top;
+        rcRight.right = rcClient.right;
+        rcRight.bottom = rcClient.bottom;
+
+        FillRect(hDC, &rcLeft, hBackgroundBrush);
+        FillRect(hDC, &rcRight, hBackgroundBrush);
+
+        DeleteObject(hBackgroundBrush);
+
+        return 1;
+    }
+
+    case WM_PAINT:{
+
+        PAINTSTRUCT ps;
+        HDC hDC = BeginPaint(hWnd, &ps);
+
+        // draw the boxes
+
+        HPEN hOriginalPen = SelectObject(hDC, hPen);
+        HFONT hOriginalFont = SelectObject(hDC, hFont);
+        SetBkColor(hDC, RGB(255, 255, 255));
+        SetTextColor(hDC, RGB(0, 0, 0));
+
+        for(int row = 0; row < 3; row++){
+            for(int col = 0; col < 3; col++){
+
+                char text[2] = {0};
+                RECT rcBox;
+                rcBox.left = rcArea.left + col * boxLen;
+                rcBox.right = rcBox.left + boxLen;
+                rcBox.top = rcArea.top + row * boxLen;
+                rcBox.bottom = rcBox.top + boxLen;
+
+                // draw the rectange
+                Rectangle(hDC, rcBox.left, rcBox.top, rcBox.right, rcBox.bottom);
+
+                // draw the symbol
+                text[0] = symbols[row][col];
+                DrawText(hDC, text, -1, &rcBox, DT_VCENTER | DT_SINGLELINE | DT_CENTER);
+
+            }
+        }
+
+        // draw the line
+        if(fGameOver)
+        {
+            SelectObject(hDC, hGameOverPen);
+            MoveToEx(hDC, lineStart.x, lineStart.y, (LPPOINT) NULL);
+            LineTo(hDC, lineEnd.x, lineEnd.y);
+        }
+
+        SelectObject(hDC, hOriginalPen);
+        SelectObject(hDC, hOriginalFont);
+        EndPaint(hWnd, &ps);
+
+        return 0;
+
+    }
+
+    case WM_CLOSE:{
+        DeleteObject(hFont);
+        DestroyWindow(hWnd);
+    }
+
+    case WM_DESTROY:
+        PostQuitMessage(TRUE);
+        return 0;
+    
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+
+}
