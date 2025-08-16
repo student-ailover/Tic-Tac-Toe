95ab223b61c2e53e4d5991a208cb8c0babc387c8
student-ailover <ayushsule1036@gmail.com>
2025-08-16 20:39:36 +0530

    dialog window will ask the user for player names

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <windows.h>
+#include <commctrl.h>
 #include <time.h>
 #include <stdlib.h>
 
-char symbols[3][3] = {
-    {' ', ' ', ' '},
-    {' ', ' ', ' '},
-    {' ', ' ', ' '}
-};
+BOOL AskPlayerNames();
+void InitGame();
+void CheckIfGameIsOver(HINSTANCE);
+BOOL AskPlayerNames();
+
+char symbols[3][3];
 int symbolCount;
 char nextSymbol, winnerSymbol;
 int boxLen;
 RECT rcArea;
 
 char windowText[50];
-
-char playerName[2][20] = {"Ayush", "Amol"};
-
+char playerName[2][20];
 BOOL fGameOver;
 
 POINT lineStart, lineEnd;
+HWND hMainWnd;
 
-void InitGame(HWND hWnd){
+#define IDM_SET_PLAYER_NAMES 1
+#include "resources.h"
+#include "window_proc.h"
+
+void InitGame(){
 
     fGameOver = FALSE;
     nextSymbol = rand() % 2? 'X' : 'O';
@@ -30,21 +35,21 @@ void InitGame(HWND hWnd){
     firstPlayer = nextSymbol == 'X'? playerName[0] : playerName[1];
     strcpy(windowText, firstPlayer);
     strcat(windowText, " will be the first one to play");
-    SetWindowText(hWnd, windowText);
+    SetWindowText(hMainWnd, windowText);
 
     symbolCount = 0;
     memset(&lineStart, 0, sizeof(POINT));
     memset(&lineEnd, 0, sizeof(POINT));
-    memset(symbols, ' ', 9);
+    memset(symbols, '\0', 9);
 
 }
 
-void CheckIfGameIsOver(HWND hWnd){
+void CheckIfGameIsOver(HINSTANCE hInst){
 
-    // rows
+    // check for rows
     for(int row = 0; row < 3; row++){
         if(symbols[row][0] == symbols[row][1] && symbols[row][1] == symbols[row][2]
-         && symbols[row][0] != ' '){
+         && symbols[row][0] != '\0'){
             winnerSymbol = symbols[row][0];
             lineStart.x = rcArea.left;
             lineStart.y = rcArea.top + boxLen * row + boxLen / 2;
@@ -54,10 +59,10 @@ void CheckIfGameIsOver(HWND hWnd){
         }
     }
 
-    // columns
+    // check for columns
     for(int col = 0; col < 3; col++){
         if(symbols[0][col] == symbols[1][col] && symbols[1][col] == symbols[2][col]
-         && symbols[0][col] != ' '){
+         && symbols[0][col] != '\0'){
             winnerSymbol = symbols[0][col];
             lineStart.x = rcArea.left + boxLen * col + boxLen / 2;
             lineStart.y = rcArea.top;
@@ -69,7 +74,7 @@ void CheckIfGameIsOver(HWND hWnd){
 
     // diagonals
     if(symbols[0][0] == symbols[1][1] && symbols[1][1] == symbols[2][2]
-        && symbols[0][0] != ' '){
+        && symbols[0][0] != '\0'){
 
         winnerSymbol = symbols[0][0];
         lineStart.x = rcArea.left;
@@ -81,7 +86,7 @@ void CheckIfGameIsOver(HWND hWnd){
     }
 
     else if(symbols[0][2] == symbols[1][1] && symbols[1][1] == symbols[2][0]
-        && symbols[0][2] != ' '){
+        && symbols[0][2] != '\0'){
 
         winnerSymbol = symbols[0][2];
         lineStart.x = rcArea.right;
@@ -92,14 +97,15 @@ void CheckIfGameIsOver(HWND hWnd){
         
     }
 
+    // if no winner is found and all the symbols are filled then Game is 'Draw'
     else if(!fGameOver && symbolCount == 9){
-        winnerSymbol = ' ';
+        winnerSymbol = '\0';
         fGameOver = TRUE;
     }
 
     if(!fGameOver) return;
 
-    RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_ERASE | RDW_UPDATENOW);
+    RedrawWindow(hMainWnd, NULL, NULL, RDW_INVALIDATE | RDW_ERASE | RDW_UPDATENOW);
 
     const char *winnerName;
 
@@ -111,41 +117,75 @@ void CheckIfGameIsOver(HWND hWnd){
         case 'O': winnerName = playerName[1];
             break;
 
-        case ' ':
-            SetWindowText(hWnd, "DRAW");
+        case '\0':
+            SetWindowText(hMainWnd, "DRAW");
             return;
 
     }
 
     strcpy(windowText, winnerName);
     strcat(windowText, " is the winner");
-    SetWindowText(hWnd, windowText);
+    SetWindowText(hMainWnd, windowText);
 
     strcpy(windowText, "Congratulations, ");
     strcat(windowText, winnerName);
     strcat(windowText, "!");
 
-    if(!PlaySound("victory.wav", NULL, SND_FILENAME | SND_ASYNC))
+    if(!PlaySound(MAKEINTRESOURCE(IDS_VICTORY), hInst, SND_RESOURCE | SND_ASYNC))
         printf("unable to play sound\n");
     
-    MessageBox(hWnd, "You are the winner", windowText, MB_OK);
+    MessageBox(hMainWnd, "You are the winner", windowText, MB_OK);
 
 }
 
-#include "window_proc.h"
+BOOL AskPlayerNames(){
+
+    // hide and disable main window
+    EnableWindow(hMainWnd, FALSE);
+    ShowWindow(hMainWnd, SW_HIDE);
+
+    // retrieve the size of the screen, create a dialog window size is (1/5)th the screen's size,
+    // and is centred on the screen
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    HWND hDlg = CreateWindow(
+        "My Dialog Class", "Enter the player names", WS_SYSMENU | WS_BORDER | WS_VISIBLE,
+        screenWidth / 2.5, screenHeight / 2.5, screenWidth / 5, screenHeight / 5,
+        NULL, NULL, NULL, NULL
+    );
+    if(!hDlg)
+    {
+        MessageBox(NULL, "UNABLE TO CREATE DIALOG WINDOW", NULL, MB_OK);
+        return FALSE;
+    }
+
+    return TRUE;
+
+}
 
 int APIENTRY WinMain(
-    HINSTANCE hInstance,
+    HINSTANCE hInst,
     HINSTANCE hPrevInstance,
     LPSTR cmdLine,
     int nCmdShow
 )
 {
 
+    HICON hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDC_APP));
+    if(!hIcon)
+    {
+        MessageBox(NULL, "UNABLE TO LOAD ICON", NULL, MB_OK);
+        return FALSE;
+    }
+
     WNDCLASS wc = {0};
+
+    // define and register main window's class
     wc.hbrBackground = CreateSolidBrush(RGB(255, 0, 0));
     wc.lpszClassName = "My Window Class";
-    wc.lpfnWndProc = WindowProc;
+    wc.lpfnWndProc = MainWndProc;
+    wc.hIcon = hIcon;
 
     if(!RegisterClass(&wc))
     {
@@ -153,20 +193,53 @@ int APIENTRY WinMain(
         return FALSE;
     }
 
-    HWND hWnd = CreateWindow(
+    // define and register dialog window's class
+    wc.hbrBackground = NULL;
+    wc.lpszClassName = "My Dialog Class";
+    wc.lpfnWndProc = DlgProc;
+    wc.hIcon = hIcon;
+
+    if(!RegisterClass(&wc))
+    {
+        MessageBox(NULL, "UNABLE TO REGISTER DIALOG CLAS", NULL, MB_OK);
+        return FALSE;
+    }
+
+    // create the main window and its menu
+    HMENU hMenu = CreateMenu();
+    AppendMenu(hMenu, MF_STRING, IDM_SET_PLAYER_NAMES, "Set Player Names");
+    hMainWnd = CreateWindow(
         "My Window Class",
-        "Tic Tac Toe",
+        NULL,
         WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_BORDER | WS_CAPTION | WS_SYSMENU | WS_SIZEBOX,
-        50, 50, 500, 400,
-        NULL, NULL, NULL, NULL
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, hMenu, hInst, NULL
     );
-    if(!hWnd)
+    if(!hMainWnd)
     {
         MessageBox(NULL, "UNABLE TO CREATE WINDOW", NULL, MB_OK);
         return FALSE;
     }
 
-    ShowWindow(hWnd, nCmdShow);
+    // try to open the save and find the player names
+    FILE *fp = fopen("player names.sav", "r");
+
+    // if unable to do so, ask the user directly through a dialog window
+    if(!fp)
+    {
+        if(!AskPlayerNames()) return FALSE;
+    }
+    else{
+
+        fscanf(fp, "%s %s", playerName[0], playerName[1]);
+        fclose(fp);
+
+        // make the main window visible
+        ShowWindow(hMainWnd, SW_NORMAL);
+
+    }
+
+    InitGame();
 
     MSG msg;
     while(GetMessage(&msg, NULL, 0, 0))
--- window_proc.h
+++ window_proc.h
@@ -1,29 +1,279 @@
-HPEN hPen;
-HPEN hGameOverPen;
-HFONT hFont;
+#ifndef WINDOW_PROC_H
+#define WINDOW_PROC_H
 
-LRESULT CALLBACK WindowProc(
+static BOOL fPlayerNamesSelected;
+static HWND hPlayerEdit[2];
+static HWND hSaveButton;
+static WNDPROC hEditDefaultProc;
+static WNDPROC hButtonDefaultProc;
+
+LRESULT CALLBACK MainWindowProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK ButtonProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK EditProc(HWND, UINT, WPARAM, LPARAM);
+
+LRESULT CALLBACK EditProc(
+    HWND hEdit,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+
+    switch (uMsg)
+    {
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_TAB:
+            if(hEdit == hPlayerEdit[0])
+                SetFocus(hPlayerEdit[1]);
+            else if(hEdit == hPlayerEdit[1]) SetFocus(hSaveButton);
+            return 0;
+        
+        }
+        break;
+    }
+
+    return hEditDefaultProc(hEdit, uMsg, wParam, lParam);
+
+}
+
+LRESULT CALLBACK ButtonProc(
+    HWND hButton,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+
+    switch (uMsg)
+    {
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_TAB:
+            SetFocus(hPlayerEdit[0]);
+            return 0;
+
+        // when the user presses Enter while the button has keyboard focuses,
+        // it programmatically generate a 'Button Click' message
+        case VK_RETURN:
+            printf("enter\n");
+            SendMessage(hButton, BM_CLICK, (WPARAM) 0, (LPARAM) 0);
+            return 0;
+        
+        default:
+            break;
+        }
+        break;
+    
+    default:
+        break;
+    }
+
+    return hButtonDefaultProc(hButton, uMsg, wParam, lParam);
+
+}
+
+LRESULT CALLBACK DlgProc(
+    HWND hDlg,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+
+    static HFONT hDlgFont;
+
+    switch (uMsg)
+    {
+
+    case WM_CREATE:{
+
+        RECT rcClient;
+        GetClientRect(hDlg, &rcClient);
+
+        // create a font for the dialog
+        hDlgFont = CreateFont(
+            rcClient.bottom / 5 - 5,
+            0,0,0,FW_DONTCARE,FALSE,FALSE,FALSE,DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
+                CLIP_DEFAULT_PRECIS,CLEARTYPE_QUALITY, VARIABLE_PITCH,TEXT("Times New Roman")
+        );
+
+        // create each edit control, set it's text font, and subclass its proc
+        for(int i = 0; i < 2; i++){
+            hPlayerEdit[i] = CreateWindow(
+                "EDIT",
+                playerName[i],
+                WS_CHILD | WS_VISIBLE | ES_UPPERCASE | WS_BORDER,
+                (rcClient.right / 5) * 2, (rcClient.bottom / 5) * (1 + i * 2),
+                (rcClient.right / 5) * 2, (rcClient.bottom / 5),
+                hDlg, NULL, NULL, NULL
+            );
+            SendMessage(hPlayerEdit[i], WM_SETFONT, (WPARAM) hDlgFont, (LPARAM) 0);
+            hEditDefaultProc = (WNDPROC) SetWindowLong(hPlayerEdit[i], GWL_WNDPROC, (LONG) EditProc);
+        }
+        
+        // if unable to create the edit controls
+        if(!hPlayerEdit[0] || !hPlayerEdit[1])
+        {
+            MessageBox(NULL, "UNABLE TO CREATE EDIT CONTROLS", NULL, MB_OK);
+            return -1;
+        }
+
+        // create a save button
+        hSaveButton = CreateWindow(
+            "BUTTON",
+            "Save",
+            WS_CHILD | WS_VISIBLE,
+            (rcClient.right - (rcClient.right / 5) * 2) / 2, (rcClient.bottom / 5) * 4,
+            (rcClient.right / 5) * 2, (rcClient.bottom / 5),
+            hDlg, NULL, NULL, NULL
+        );
+        
+        if(!hSaveButton)
+        {
+            MessageBox(NULL, "UNABLE TO CREATE SAVE BUTTON", NULL, MB_OK);
+            return -1;
+        }
+
+        // subclass its procedure
+        hButtonDefaultProc = (WNDPROC) SetWindowLong(hSaveButton, GWL_WNDPROC, (LONG) ButtonProc);
+
+        // set keyboard focus to first edit control
+        SetFocus(hPlayerEdit[0]);
+
+        return 0;
+    }
+
+    case WM_LBUTTONDOWN:{
+        SetFocus(hDlg);
+        return 0;
+    }
+
+    case WM_PAINT:{
+
+        PAINTSTRUCT ps;
+        HDC hDC = BeginPaint(hDlg, &ps);
+
+        RECT rcDlg;
+        GetClientRect(hDlg, &rcDlg);
+
+        HFONT hOriginalFont = SelectObject(hDC, hDlgFont);
+
+        RECT rcText;
+        rcText.left = 0;
+        rcText.right = (rcDlg.right / 5) * 2;
+        rcText.top = (rcDlg.bottom / 5) * 1;
+        rcText.bottom = (rcDlg.bottom / 5) * 2;
+        DrawText(hDC, "Player 1:", -1, &rcText, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+        rcText.top += (rcDlg.bottom / 5) * 2;
+        rcText.bottom += (rcDlg.bottom / 5) * 2;
+        DrawText(hDC, "Player 2:", -1, &rcText, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+        SelectObject(hDC, hOriginalFont);
+        EndPaint(hDlg, &ps);
+
+        return 0;
+    }
+
+    case WM_COMMAND:{
+
+        // change made here
+        if(HIWORD(wParam) != BN_CLICKED) return 0;
+
+        // when save button is clicked
+        // if either edit control is empty then ignore the click
+        else if(!GetWindowTextLength(hPlayerEdit[0]) || !GetWindowTextLength(hPlayerEdit[1]))
+            return 0;
+
+        fPlayerNamesSelected = TRUE;
+
+        // retrieve the player names from their respective edit controls and save in the
+        // "player names.sav" file
+        GetWindowText(hPlayerEdit[0], playerName[0], sizeof(playerName[0]));
+        GetWindowText(hPlayerEdit[1], playerName[1], sizeof(playerName[0]));
+        printf("%s\n", playerName[0]);
+
+        FILE *fp = fopen("player names.sav", "w");
+        if(!fp)
+        {
+            MessageBox(NULL, "UNABLE TO SAVE PLAYER NAMES", NULL, MB_OK);
+            return 0;
+        }
+        fprintf(fp, "%s %s", playerName[0], playerName[1]);
+        fclose(fp);
+        
+        // set the proper title for the main window
+        char mainWndTitle[50];
+        strcpy(mainWndTitle, "Now, it is ");
+        strcat(mainWndTitle, playerName[nextSymbol == 'X' ? 0 : 1]);
+        strcat(mainWndTitle, "'s turn");
+        SetWindowText(hMainWnd, mainWndTitle);
+
+        // enable and show the main window, and then destroy the dialog
+        EnableWindow(hMainWnd, TRUE);
+        ShowWindow(hMainWnd, SW_NORMAL);
+        DestroyWindow(hDlg);
+
+        return 0;
+    }
+
+    case WM_CLOSE:{
+
+        DestroyWindow(hDlg);
+        PostQuitMessage(TRUE);
+
+        return 0;
+    }
+
+    case WM_DESTROY:{
+
+        if(!fPlayerNamesSelected) return 0;
+        DeleteObject(hDlgFont);
+        return 0;
+
+    }
+    
+    default:
+        return DefWindowProc(hDlg, uMsg, wParam, lParam);
+    }
+
+}
+
+LRESULT CALLBACK MainWndProc(
     HWND hWnd,
     UINT uMsg,
     WPARAM wParam,
     LPARAM lParam
 ){
 
+    static HINSTANCE hInst;
+    static HPEN hBorderPen, hGameOverPen;
+    static HFONT hSymbolFont;
+
     switch (uMsg)
     {
     case WM_CREATE:{
 
+        hInst = ((CREATESTRUCT*) lParam)->hInstance;
+
         srand(time(0));
         InitGame(hWnd);
-        hPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 0));
+
+        hBorderPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 0));
         hGameOverPen = CreatePen(PS_SOLID, 15, RGB(0, 0, 0));
 
         return 0;
     }
 
+    // after the main window gets resized, destroy the existsing font
+    // and create a new, bigger font
     case WM_SIZE:{
 
-        if(hFont) DeleteObject(hFont);
+        if(hSymbolFont) DeleteObject(hSymbolFont);
 
         boxLen = (LOWORD(lParam) / 3 < HIWORD(lParam) / 3) ? (LOWORD(lParam) / 3) : (HIWORD(lParam) / 3);
         rcArea.left = (LOWORD(lParam) - boxLen * 3) / 2;
@@ -31,12 +281,27 @@ LRESULT CALLBACK WindowProc(
         rcArea.right = rcArea.left + boxLen * 3;
         rcArea.bottom = rcArea.top + boxLen * 3;
 
-        hFont = CreateFont(boxLen - 10,0,0,0,FW_DONTCARE,FALSE,FALSE,FALSE,DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
+        if(hSymbolFont) DeleteObject(hSymbolFont);
+        hSymbolFont = CreateFont(boxLen - 10,0,0,0,FW_DONTCARE,FALSE,FALSE,FALSE,DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
                 CLIP_DEFAULT_PRECIS,CLEARTYPE_QUALITY, VARIABLE_PITCH,TEXT("Impact"));
 
-        SelectObject(GetDC(hWnd), hFont);
+        SelectObject(GetDC(hWnd), hSymbolFont);
 
         RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW | RDW_ERASE);
+
+        return 0;
+    }
+
+    case WM_COMMAND:{
+
+        printf("here\n");
+        switch(LOWORD(wParam)){
+
+            case IDM_SET_PLAYER_NAMES:
+                AskPlayerNames();
+                break;
+
+        }
 
         return 0;
     }
@@ -62,12 +327,12 @@ LRESULT CALLBACK WindowProc(
         int row = (cursorPos.y - rcArea.top) / boxLen;
         int col = (cursorPos.x - rcArea.left) / boxLen;
         
-        if(symbols[row][col] != ' ') return 0;
+        if(symbols[row][col] != '\0') return 0;
 
         symbols[row][col] = nextSymbol;
         symbolCount++;
         
-        CheckIfGameIsOver(hWnd);
+        CheckIfGameIsOver(hInst);
         RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
 
         if(!fGameOver){
@@ -82,6 +347,7 @@ LRESULT CALLBACK WindowProc(
         return 0;
     }
 
+    // send just before WM_PAINT
     case WM_ERASEBKGND:{
 
         HDC hDC = (HDC) wParam;
@@ -102,14 +368,29 @@ LRESULT CALLBACK WindowProc(
         rcRight.right = rcClient.right;
         rcRight.bottom = rcClient.bottom;
 
+        RECT rcTop;
+        rcTop.left = rcArea.left;
+        rcTop.right = rcArea.right;
+        rcTop.top = rcClient.top;
+        rcTop.bottom = rcArea.top;
+
+        RECT rcBottom;
+        rcBottom.left = rcArea.left;
+        rcBottom.right = rcArea.right;
+        rcBottom.top = rcArea.top;
+        rcBottom.bottom = rcClient.bottom;
+
         FillRect(hDC, &rcLeft, hBackgroundBrush);
         FillRect(hDC, &rcRight, hBackgroundBrush);
+        FillRect(hDC, &rcTop, hBackgroundBrush);
+        FillRect(hDC, &rcBottom, hBackgroundBrush);
 
         DeleteObject(hBackgroundBrush);
 
         return 1;
     }
 
+    // send invalidate region of client is to be painted
     case WM_PAINT:{
 
         PAINTSTRUCT ps;
@@ -117,8 +398,8 @@ LRESULT CALLBACK WindowProc(
 
         // draw the boxes
 
-        HPEN hOriginalPen = SelectObject(hDC, hPen);
-        HFONT hOriginalFont = SelectObject(hDC, hFont);
+        HPEN hOriginalPen = SelectObject(hDC, hBorderPen);
+        HFONT hOriginalFont = SelectObject(hDC, hSymbolFont);
         SetBkColor(hDC, RGB(255, 255, 255));
         SetTextColor(hDC, RGB(0, 0, 0));
 
@@ -159,11 +440,15 @@ LRESULT CALLBACK WindowProc(
     }
 
     case WM_CLOSE:{
-        DeleteObject(hFont);
         DestroyWindow(hWnd);
+        return 0;
     }
 
+    // send after the window is destroy, post a WM_QUIT which will end the message loop
     case WM_DESTROY:
+        DeleteObject(hSymbolFont);
+        DeleteObject(hBorderPen);
+        DeleteObject(hGameOverPen);
         PostQuitMessage(TRUE);
         return 0;
     
@@ -172,3 +457,5 @@ LRESULT CALLBACK WindowProc(
     }
 
 }
+
+#endif
